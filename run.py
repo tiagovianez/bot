f4f0b6297764581fb60ac6f9b1cc9a9c26f45344
tiagovianez <tiagovianez@live.com>
2022-03-25 18:20:07 -0300

    booking-packages

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ with Booking() as bot:
     bot.land_first_page()
     bot.change_currency(currency='USD')
     bot.select_place_to_go('Salvador')
-    bot.select_dates(check_in_date='2022-03-24',
+    bot.select_dates(check_in_date='2022-03-25',
                      check_out_date='2022-03-29')
-    bot.select_adults()
-
-    bot.descrease_adults_element()
+    bot.select_adults(1)
+    bot.click_search()
+    bot.apply_filtrations()
