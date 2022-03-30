af66a3280fbb90cf3c65fdd4fad250c1c3b7c7ea
tiagovianez <tiagovianez@live.com>
2022-03-30 16:22:18 -0300

    update tasks

--- run.py
+++ run.py
@@ -1,11 +1,27 @@
 from booking.booking import Booking
 
-with Booking() as bot:
-    bot.land_first_page()
-    bot.change_currency(currency='USD')
-    bot.select_place_to_go('Salvador')
-    bot.select_dates(check_in_date='2022-03-25',
-                     check_out_date='2022-03-29')
-    bot.select_adults(1)
-    bot.click_search()
-    bot.apply_filtrations()
+try:
+    with Booking() as bot:
+        # a = 2 / 0
+        bot.land_first_page()
+        bot.change_currency(currency='USD')
+        bot.select_place_to_go('Salvador')
+        bot.select_dates(check_in_date='2022-03-30',
+                         check_out_date='2022-03-31')
+        bot.select_adults(1)
+        bot.click_search()
+        bot.apply_filtrations()
+        print(len(bot.report_results()))
+
+except Exception as e:
+    if 'in PATH' in str(e):
+        print(
+            'You are trying to run the bot from command line \n'
+            'Please add to PATH your Selenium Drivers \n'
+            'Windows: \n'
+            '   set PATH=%PATH;C:path-to-your-folder \n \n'
+            'Linux: \n'
+            'PATH=$PATH:"/home/tiago/Documents/Projects/bot/run.py \n'
+        )
+    else:
+        raise
