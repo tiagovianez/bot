e009f3b6b94538434fdf3e22172d6e14023066ea
tiagovianez <tiagovianez@live.com>
2022-03-30 16:23:32 -0300

    sort_price_lowest_first

--- booking/booking_filtration.py
+++ booking/booking_filtration.py
@@ -4,13 +4,26 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
-class BookFiltration:
+class BookingFiltration:
     def __init__(self, driver:WebDriver):
         self.driver = driver
 
-    def apply_star_rating(self):
+    def apply_star_rating(self, *star_values):
         star_filtration_box = self.driver.find_element_by_css_selector(
             'div[data-filters-group="class"]'
         )
-        star_child_elements = star_filtration_box.find_elements_by_ccs_selector('*')
-        print(len(star_child_elements))
+        star_child_elements = star_filtration_box.find_elements_by_css_selector('*')
+
+        for star_value in star_values:
+            for star_element in star_child_elements:
+                if str(star_element.get_attribute('innerHTML')).strip() == f'{star_value} stars':
+                    star_element.click()
+
+
+    def sort_price_lowest_first(self):
+        element = self.driver.find_element_by_css_selector(
+            'li[data-id="price"]'
+        )
+        element.click()
+
+
